c736230089ac30d2711721bce65dbab5a536bd3e
sokomishalov <vtb218497@corp.dev.vtb>
2019-09-26 19:59:04 +0300

    added contract

--- commons-core/src/main/kotlin/ru/sokomishalov/commons/core/string/StringUtils.kt
+++ commons-core/src/main/kotlin/ru/sokomishalov/commons/core/string/StringUtils.kt
@@ -1,9 +1,18 @@
-@file:Suppress("unused")
+@file:Suppress("unused", "NOTHING_TO_INLINE")
 
 package ru.sokomishalov.commons.core.string
+
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.contract
 
 /**
  * @author sokomishalov
  */
 
-fun CharSequence?.isNotNullOrBlank(): Boolean = this.isNullOrBlank().not()
+@UseExperimental(ExperimentalContracts::class)
+inline fun CharSequence?.isNotNullOrBlank(): Boolean {
+    contract {
+        returns(true) implies (this@isNotNullOrBlank != null)
+    }
+    return (this == null || this.isBlank()).not()
+}
