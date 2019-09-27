fbec201c295dccc05fd808c26cc651c55bfe0c0b
sokomishalov <vtb218497@corp.dev.vtb>
2019-09-27 16:45:17 +0300

    cache improvements (2)

--- commons-spring/src/main/kotlin/ru/sokomishalov/commons/spring/cache/CacheManagerService.kt
+++ commons-spring/src/main/kotlin/ru/sokomishalov/commons/spring/cache/CacheManagerService.kt
@@ -4,6 +4,7 @@ package ru.sokomishalov.commons.spring.cache
 
 import org.springframework.cache.Cache
 import org.springframework.cache.CacheManager
+import ru.sokomishalov.commons.core.common.unit
 
 
 /**
@@ -16,15 +17,7 @@ class CacheManagerService(
 
     private fun getCache(cacheName: String): Cache? = cacheManager.getCache(cacheName)
 
-    override suspend fun <T> get(cacheName: String, key: String): T? {
-        return getCache(cacheName)?.get(key)?.get() as T?
-    }
-
-    override suspend fun <T> put(cacheName: String, key: String, value: T) {
-        getCache(cacheName)?.put(key, value)
-    }
-
-    override suspend fun evict(cacheName: String, key: String) {
-        getCache(cacheName)?.evict(key)
-    }
+    override suspend fun <T> get(cacheName: String, key: String): T? = getCache(cacheName)?.get(key)?.get() as T?
+    override suspend fun <T> put(cacheName: String, key: String, value: T) = getCache(cacheName)?.put(key, value).unit()
+    override suspend fun evict(cacheName: String, key: String) = getCache(cacheName)?.evict(key).unit()
 }
