68bc383ebf43d47f5cc35c06c750ff408ad800fe
sokomishalov <vtb218497@corp.dev.vtb>
2019-12-09 13:26:11 +0300

    1.0.26

--- commons-core/src/main/kotlin/ru/sokomishalov/commons/core/log/Sl4jExtensions.kt
+++ commons-core/src/main/kotlin/ru/sokomishalov/commons/core/log/Sl4jExtensions.kt
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2019-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ru.sokomishalov.commons.core.log
 
 import org.slf4j.Logger
@@ -6,12 +21,12 @@ import org.slf4j.Logger
  * @author sokomishalov
  */
 
-fun Logger.info(lazyMessage: () -> String) = info(lazyMessage())
+fun Logger.info(lazyMessage: () -> String) = if (isInfoEnabled) info(lazyMessage()) else Unit
 
-fun Logger.debug(lazyMessage: () -> String) = debug(lazyMessage())
+fun Logger.debug(lazyMessage: () -> String) = if (isDebugEnabled) debug(lazyMessage()) else Unit
 
-fun Logger.warn(lazyMessage: () -> String) = warn(lazyMessage())
+fun Logger.warn(lazyMessage: () -> String) = if (isWarnEnabled) warn(lazyMessage()) else Unit
 
-fun Logger.trace(lazyMessage: () -> String) = trace(lazyMessage())
+fun Logger.trace(lazyMessage: () -> String) = if (isTraceEnabled) trace(lazyMessage()) else Unit
 
-fun Logger.error(throwable: Throwable, lazyMessage: () -> String) = error(lazyMessage(), throwable)
+fun Logger.error(throwable: Throwable, lazyMessage: () -> String) = if (isErrorEnabled) error(lazyMessage(), throwable) else Unit
