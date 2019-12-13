a3e8b0058e7b4718a71a4a09a312cb358650c96c
sokomishalov <vtb218497@corp.dev.vtb>
2019-12-13 17:28:56 +0300

    aop fixes

---
+++ commons-spring/src/main/kotlin/ru/sokomishalov/commons/spring/autoconfigure/AopDistributedLocksConfiguration.kt
@@ -0,0 +1,22 @@
+@file:Suppress("SpringJavaInjectionPointsAutowiringInspection")
+
+package ru.sokomishalov.commons.spring.autoconfigure
+
+import org.aspectj.lang.reflect.Advice
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import ru.sokomishalov.commons.distributed.locks.DistributedLockProvider
+import ru.sokomishalov.commons.distributed.locks.WithDistributedLockAspect
+
+@Configuration(proxyBeanMethods = true)
+@ConditionalOnClass(Advice::class)
+class AopDistributedLocksConfiguration {
+
+    @Bean
+    @ConditionalOnBean(DistributedLockProvider::class)
+    fun aspect(lockProvider: DistributedLockProvider): WithDistributedLockAspect {
+        return WithDistributedLockAspect(lockProvider)
+    }
+}
